2253578ac6254d22257b8622e635b2f992724bc5
msrgoooo <m92575363tt@gmail.com>
2026-09-24 19:22:22 +0300

    Сделана п1.2.1

--- 112-blink/main.c
+++ 112-blink/main.c
@@ -1,7 +1,5 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include "hardware/regs/addressmap.h"
-#include "hardware/regs/sio.h"
 
 const uint LED_PIN = 25;
 
@@ -10,15 +8,11 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    volatile uint32_t *gpio_out_set = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
-    volatile uint32_t *gpio_out_clr = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
-    const uint32_t led_mask = 1u << LED_PIN;
-
     while (1)
     {
-        *gpio_out_set = led_mask;
+        gpio_put(LED_PIN, 1);
         sleep_ms(250);
-        *gpio_out_clr = led_mask;
+        gpio_put(LED_PIN, 0);
         sleep_ms(1000);
     }
 }
